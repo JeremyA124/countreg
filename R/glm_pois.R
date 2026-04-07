bc4377c375caaf7030d76aacc79e18bc99231766
JeremyA124 <artigajeremy7@gmail.com>
2026-04-07 19:27:30 -0400

    glm_pois() bug fixes
    Fixed bugs for glm_pois()

--- R/glm_pois.R
+++ R/glm_pois.R
@@ -14,7 +14,7 @@ glm_pois <- function(data,
   repeat{
     eta <- offset + X %*% betas
     pred.means <- exp(eta)
-    W <- diag(pred.means)
+    W <- diag(as.vector(pred.means))
     z <- eta+(log.lambdas-pred.means)/pred.means
     inv.Hess <- solve(t(X)%*%W%*%X)
     betas.new <- inv.Hess%*%t(X)%*%W%*%z
@@ -26,24 +26,28 @@ glm_pois <- function(data,
     }
   }
 
-  fit.dat <- as.data.frame(cbind(betas, std.error))
+  fit.dat <- data.frame(betas=betas,std.error=std.error)
 
   # Coefficient Pvals and Confidence Intervals
   #############################################
+  Zs <- rep(NA, times = nrow(fit.dat))
   p.vals <- rep(NA, times = nrow(fit.dat))
   asymp.CI.lower <- rep(NA, times = nrow(fit.dat))
   asymp.CI.higher <- rep(NA, times = nrow(fit.dat))
-  SEs <- sqrt(inv.Hess[i,i])
   for(i in 1:nrow(fit.dat)){
-    SE <- SEs[i,i]
-    Z <- fit.dat$betas[i]/SE
-    p.vals[i] <- 2*(1-pnorm(abs(Z)))
+    SE <- sqrt(inv.Hess[i,i])
+    Zs[i] <- fit.dat$betas[i]/SE
+    p.vals[i] <- 2*(1-pnorm(abs(Zs[i])))
     crit <- qnorm(0.975)
     asymp.CI.lower[i] <- fit.dat$betas[i]-crit*SE
     asymp.CI.higher[i] <- fit.dat$betas[i]+crit*SE
   }
 
-  fit.dat <- as.data.frame(cbind(fit.dat, asymp.CI.lower, asymp.CI.higher, p.vals))
+  fit.dat <- data.frame(fit.dat,
+                        Z=Zs,
+                        asymp.CI.lower=asymp.CI.lower,
+                        asymp.CI.higher=asymp.CI.higher,
+                        p.vals=p.vals)
 
   return(fit.dat)
 
